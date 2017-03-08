3435ce0bcae47b856ff4ae548d3486b3679a2698
RoyalBingBong <david-k@mail.de>
2017-03-08 19:23:34 +0100

    Fixed: main.js readded app.quit()...whoops

--- src/main.js
+++ src/main.js
@@ -17,7 +17,7 @@ app.on('window-all-closed', () => {
   // On OS X it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
   // if (process.platform != 'darwin') {
-  //   app.quit()
+  app.quit()
   // }
 })
 
